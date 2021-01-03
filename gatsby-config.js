b1dbc0859676c7acb6de79560a2cfe7a45df9539
hiro08gh <hiro08gh@gmail.com>
2021-01-03 18:38:22 +0900

    add: styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     "gatsby-plugin-sass",
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
