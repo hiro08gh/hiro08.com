5a8786584eae4438021cfd55f4cb9032d059a0e7
hiro08gh <hiro08gh@gmail.com>
2021-01-05 18:15:22 +0900

    add: Meta data component

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,17 +1,23 @@
 import React from "react"
+import { Meta } from "../shared/Meta"
 import "../../styles/reset.scss"
 
 import Header from "../header"
 
 type Props = {
   children: React.childeNode
+  metaTitle?: string
+  metaDescription?: string
+  metaImage?: string
 }
 
 export const MainLayout: React.FC<Props> = props => {
+  const { children, metaTitle, metaDescription, metaImage } = props
   return (
-    <div className="main">
+    <>
+      <Head metaTitle={metaTitle} metaDescription={metaDescription} metaImage={metaImage} />
       <Header siteTitle="hiro08.dev" />
-      <main>{props.children}</main>
-    </div>
+      <main>{children}</main>
+    </>
   )
 }
