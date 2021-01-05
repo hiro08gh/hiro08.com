94f66037545f265d383307a0ab9968b7995d9e66
hiro08gh <hiro08gh@gmail.com>
2021-01-05 18:40:03 +0900

    add: RSS feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,6 +30,58 @@ module.exports = {
         trackingId: process.env.TRACKING_ID,
       },
     },
+    // RSSフィードの作成
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMicrocmsBlog } }) => {
+              return allMicrocmsBlog.edges.map(edge => {
+                return Object.assign({}, edge.node, {
+                  description: edge.node.description,
+                  date: edge.node.createdAt,
+                  url: site.siteMetadata.siteUrl + "/blog/" + edge.node.blogId,
+                  guid: site.siteMetadata.siteUrl + "/blog/" + edge.node.blogId,
+                })
+              })
+            },
+            query: `
+              {
+                allMicrocmsBlog(
+                  sort: { fields: [createdAt], order: DESC },
+                ) {
+                  edges {
+                    node {
+                      id
+                      title
+                      description
+                      blogId
+                      createdAt
+                    }
+                  }
+                }
+              }
+            `,
+            // blogフォルダにfeed.xmlを作成
+            output: "/feed.xml",
+            title: "hiro08.dev",
+          },
+        ],
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
