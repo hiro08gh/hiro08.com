b3357dec4f7eb2b1a9d8fea42b1fbd6967cef77b
hiro08gh <hiro08gh@gmail.com>
2021-01-09 18:56:00 +0900

    add: Logo image

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -8,13 +8,31 @@ type Props = {
 
 const Component = (props) => (
   <h1 className={props.className}>
-    <Link to="/">hiro08.dev</Link>
+    <Link to="/">
+      <div className="row">
+        <img
+          src="https://images.microcms-assets.io/protected/ap-northeast-1:2c74e029-93cb-43ad-9026-3e1fd330c060/service/jamzii-blog/media/39504660.png"
+          alt="ロゴイメージ"
+        />
+        <span>hiro08.dev</span>
+      </div>
+    </Link>
   </h1>
 );
 
 export const Logo = styled(Component)`
+  .row {
+    display: flex;
+    align-items: center;
+  }
   a {
     color: #33333;
     font-size: 18px;
+  }
+  img {
+    width: 40px;
+    height: 40px;
+    border-radius: 100%;
+    margin-right: 8px;
   }
 `;
