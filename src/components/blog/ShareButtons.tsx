2c7e8ba8b1c7ba28ca6a50b67acd3bc1bff7bca4
hiro08gh <hiro08gh@gmail.com>
2021-01-10 12:43:50 +0900

    add: social share buttons

---
+++ src/components/blog/ShareButtons.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from 'styled-components';
+import { FacebookShareButton, HatenaShareButton, TwitterShareButton, FacebookIcon, HatenaIcon, TwitterIcon } from 'react-share';
+
+type Props = {
+  className: string;
+  path: string;
+  title?: string;
+};
+
+const Component: React.FC = (props) => (
+  <div className={props.className}>
+    <div className="share-buttons">
+      <TwitterShareButton url={`https://hiro08.dev/blog/${props.path}`} title={`${props.title}`} className="btn">
+        <TwitterIcon size={42} round={true} />
+      </TwitterShareButton>
+      <FacebookShareButton url={`https://hiro08.dev/blog/${props.path}`} title={`${props.title}`} className="btn">
+        <FacebookIcon size={42} round={true} />
+      </FacebookShareButton>
+      <HatenaShareButton url={`https://hiro08.dev/blog/${props.path}`} title={`${props.title}`} className="btn">
+        <HatenaIcon size={42} round={true} />
+      </HatenaShareButton>
+    </div>
+    <span className="share__text">👈この記事をシェアする</span>
+  </div>
+);
+
+export const ShareButtons = styled(Component)`
+  display: flex;
+  align-items: center;
+  margin-top: 24px;
+  .share-buttons {
+    margin-top: 16px;
+    margin-right: 16px;
+  }
+
+  .share__text {
+    font-size: 20px;
+  }
+  .btn {
+    margin-right: 8px;
+  }
+`;
--- src/pages/blog/{microcmsBlog.blogId}.tsx
+++ src/pages/blog/{microcmsBlog.blogId}.tsx
@@ -2,10 +2,11 @@ import React, { useEffect } from 'react';
 import { graphql, Link } from 'gatsby';
 import Prism from 'prismjs';
 import { MainLayout } from '../../components/layouts/MainLayout';
-import { MicrocmsBlog } from '../../graphqlTypes';
 import { Author } from '../../components/blog/Author';
 import { Content } from '../../components/blog/Content';
 import { Inner } from '../../components/shared/Inner';
+import { MicrocmsBlog } from '../../graphqlTypes';
+import { ShareButtons } from '../../components/blog/ShareButtons';
 
 type Props = {
   data: {
@@ -23,6 +24,7 @@ const BlogPage: React.FC<Props> = ({ data: { microcmsBlog } }) => {
       <Inner>
         {blog.title}
         <Content body={blog.body} />
+        <ShareButtons path={blog.blogId} title={blog.title} />
         <Author name={blog.author.name} description={blog.author.description} image={blog.author.image.url} />
       </Inner>
     </MainLayout>
