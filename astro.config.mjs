3aa017443fd9510003ad0b465c7dac42befee92b
hiro08gh <hiro08gh@gmail.com>
2024-04-22 21:57:25 +0900

    add: sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
+  site: "https://hiro08.com/",
 });
