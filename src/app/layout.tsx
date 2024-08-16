d4286c3172dc04dd9aa90171e7257afc1663639a
hiro08gh <hiro08gh@gmail.com>
2024-08-16 18:43:14 +0900

    add: cache time control

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "hiro08gh",
+	title: "hiro08gh website",
 	description: "hiro08gh web site",
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import { Top } from "@/app/_components/Top";
 import { getAbout } from "@/libs/microcms";
 
-export const revalidate = 0;
+export const revalidate = process.env.CACHE_TIME
+	? Number(process.env.CACHE_TIME)
+	: 3600;
+
 export default async function Page() {
 	const about = await getAbout();
 
