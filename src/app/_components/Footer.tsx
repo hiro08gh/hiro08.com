862df3af486f06dbdf63c1f0c28794b5259ac106
hiro08gh <hiro08gh@gmail.com>
2024-08-17 12:09:02 +0900

    add: current year

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -1,7 +1,9 @@
+import { currentYear } from "@/libs/date";
+
 export const Footer: React.FC = () => {
 	return (
 		<footer className="text-center text-sm text-gray my-4">
-			© 2024 hiro08.com.
+			© {currentYear()} hiro08.com.
 		</footer>
 	);
 };
