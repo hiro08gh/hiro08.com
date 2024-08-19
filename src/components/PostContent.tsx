5e2358f519d391f1028df843f09cc9b3601bf8fa
hiro08gh <hiro08gh@gmail.com>
2024-08-19 23:46:45 +0900

    add: update time

--- src/components/PostContent.tsx
+++ src/components/PostContent.tsx
@@ -1,7 +1,7 @@
 import { formatDate } from "@/libs/date";
 import { marked } from "@/libs/marked";
 import type { PostDetailType } from "@/libs/microcms";
-import { BookmarkIcon } from "@radix-ui/react-icons";
+import { BookmarkIcon, UpdateIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
 import { RichEditorToMarkdownParser } from "rich-editor-to-markdown-parser";
 
@@ -15,8 +15,14 @@ export const PostContent: React.FC<Props> = (props) => {
 	return (
 		<div className="mb-10">
 			<h2 className="text-2xl font-bold mb-4">{post.title}</h2>
-			<div className="mb-10 flex gap-3">
+			<div className="mb-10 flex items-center gap-3">
 				<div>{formatDate(post.createdAt)}</div>
+				{formatDate(post.updatedAt) !== formatDate(post.createdAt) && (
+					<div className="flex items-center gap-1">
+						<UpdateIcon />
+						{formatDate(post.updatedAt)}
+					</div>
+				)}
 				<div className="flex gap-2">
 					{post.tags.map((tag) => (
 						<div key={tag.id} className="flex items-center">
