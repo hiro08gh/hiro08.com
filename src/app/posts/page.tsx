19af6e96be481f9007305e9ae18f0d26163f47af
hiro08gh <hiro08gh@gmail.com>
2024-08-20 16:40:07 +0900

    add: pagination

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,3 +1,4 @@
+import { Pagination } from "@/components/Pagination";
 import { PostCard } from "@/components/PostCard";
 import { getPosts } from "@/libs/microcms";
 import type { Metadata } from "next";
@@ -14,6 +15,7 @@ export default async function Page() {
 			{posts.contents.map((post) => (
 				<PostCard post={post} key={post.id} />
 			))}
+			<Pagination totalCount={10} />
 		</div>
 	);
 }
---
+++ src/components/Pagination.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+type Props = {
+	totalCount: number;
+};
+
+export const Pagination: React.FC<Props> = ({ totalCount }) => {
+	const perPage = Math.ceil(totalCount / 10);
+
+	const range = (start: number, end: number) =>
+		[...Array(end - start + 1)].map((_, i) => {
+			return {
+				key: crypto.randomUUID(),
+				number: start + i,
+			};
+		});
+
+	return (
+		<ul className="flex items-center gap-5 justify-center mt-20">
+			{range(1, perPage).map((page) => (
+				<li key={page.key} className="text-center">
+					<Link href="/posts">{page.number}</Link>
+				</li>
+			))}
+		</ul>
+	);
+};
